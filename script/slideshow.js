30aa64e284724c7bf86bfab5f8c3a3b7adc3375e
Tiddi <tidemann.tiddi@hotmail.com>
2016-09-29 10:54:08 +0200

    Flyttet filer

--- script/slideshow.js
+++ script/slideshow.js
@@ -5,19 +5,14 @@ window.onload = oppstart;
 	function oppstart(){
 		document.getElementById("btnLeggTilBilde").onclick = leggTilBilde;
 		bildeURLElement = document.getElementById("bildeInput");
-
-		console.log(bildeURLElement);
 		document.getElementById("bilde").src = bildeArray[bildeTeller];
-		setInterval(byttBilde,1000)
-
+		setInterval(byttBilde,3000)
 }
 
 /******* SLIDESHOW *********/
 	function leggTilBilde() {
 		if(bildeURLElement.value !== "") {
-			console.log("test")
 			bildeArray.push(bildeURLElement.value);
-			console.log(bildeArray);
 			bildeURLElement.value = "";
 		}
 }
