c2f4a2c5bd9d79f3c68d99a35f4fb8da1d308d6c
Tiddi <tidemann.tiddi@hotmail.com>
2016-10-15 18:43:37 +0200

    samlet funksjoner

---
+++ lagside/lagside.js
@@ -0,0 +1,65 @@
+//Farge elementer
+var colorRed, colorGreen, colorBlue,colorAlpha;
+var colorRedElement  = document.getElementById("red");
+var colorGreenElement= document.getElementById("green");
+var colorBlueElement = document.getElementById("blue");
+var colorAlphaElement = document.getElementById("alpha");
+
+var colorVisualiser = document.getElementById("fargeEndrer")
+var fargeFraFargevelger;
+var skalSetteFarge = false;
+
+//Border elemeneter
+var borderStyleSelektorElement = document.getElementById("borderStyleSelektor")
+var borderVisualiser = document.getElementById("borderVisualiser")
+var borderTykkelse = document.getElementById("borderTykkelse")
+
+//brukerstyrte funksjoner
+colorVisualiser.click = settFarge;
+colorRedElement.oninput = oppdaterfarge;
+colorGreenElement.oninput = oppdaterfarge;
+colorBlueElement.oninput = oppdaterfarge;
+colorAlphaElement.oninput = oppdaterfarge;
+borderTykkelse.oninput = oppdaterBorder
+borderStyleSelektorElement.onchange = oppdaterBorder
+
+settVerdier(borderTykkelse,0,10,2)
+settVerdier(colorRedElement,0,255,255)
+settVerdier(colorGreenElement,0,255,0)
+settVerdier(colorBlueElement,0,255,255)
+settVerdier(colorAlphaElement,0,100,100)
+
+oppdaterfarge()
+oppdaterBorder()
+
+function settVerdier(element,min,max,value) {
+	element.max = max;
+	element.min = min;
+	element.value = value;
+}
+
+function settFarge() {
+    skalSetteFarge = true
+}
+
+function giFarge(event) {
+    if(skalSetteFarge === true){
+        this.style.backgroundColor = fargeFraFargevelger;
+        skalSetteFarge = false;
+    }
+}
+
+function oppdaterfarge() {
+    colorRed = colorRedElement.value;
+    colorGreen = colorGreenElement.value;
+    colorBlue = colorBlueElement.value;
+	colorAlpha = colorAlphaElement.value/100;
+    fargeFraFargevelger = "rgba(" + colorRed + "," + colorGreen + "," + colorBlue + "," + colorAlpha +")"
+
+    colorVisualiser.style.backgroundColor = fargeFraFargevelger;
+}
+
+function oppdaterBorder() {
+	borderVisualiser.style.border = borderStyleSelektorElement.options[borderStyleSelektorElement.selectedIndex].text
+	borderVisualiser.style.borderWidth  = borderTykkelse.value + "px"
+}
--- script/lagside.js
+++ script/lagside.js
@@ -1,67 +1,70 @@
 //variabler for range bar elementer
-			var colorRedElement = document.getElementById("red");
-			var colorGreenElement = document.getElementById("green");
-			var colorBlueElement = document.getElementById("blue");
+var colorRedElement = document.getElementById("red");
+var colorGreenElement = document.getElementById("green");
+var colorBlueElement = document.getElementById("blue");
 
-			//variabler for verdien til range bar elementer
-			var colorRed;
-			var colorGreen;
-			var colorBlue;
-			var fargeFraFargevelger;
-			var skalSetteFarge = false;
-			window.onload = oppstart;
-			function oppstart(){
-				colorRedElement = document.getElementById("red");
-				colorGreenElement = document.getElementById("green");
-				colorBlueElement = document.getElementById("blue");
+//variabler for verdien til range bar elementer
+var colorRed;
+var colorGreen;
+var colorBlue;
+var fargeFraFargevelger;
+var skalSetteFarge = false;
+window.onload = oppstart;
+
+function oppstart() {
+    colorRedElement = document.getElementById("red");
+    colorGreenElement = document.getElementById("green");
+    colorBlueElement = document.getElementById("blue");
 
 
 
 
-			    document.getElementById("boks").onmousedown = settFarge;
-				document.getElementById("boks2").onmousedown = giFarge;
-				document.getElementById("boks3").onmousedown = giFarge;
-				document.getElementById("boks4").onmousedown = giFarge;
-				document.getElementById("boks5").onmousedown = giFarge;
-				document.getElementById("boks6").onmousedown = giFarge;
+    document.getElementById("boks").onmousedown = settFarge;
+    document.getElementById("boks2").onmousedown = giFarge;
+    document.getElementById("boks3").onmousedown = giFarge;
+    document.getElementById("boks4").onmousedown = giFarge;
+    document.getElementById("boks5").onmousedown = giFarge;
+    document.getElementById("boks6").onmousedown = giFarge;
 
-				colorRedElement.onmousemove = oppdaterfarge;
-				colorGreenElement.onmousemove = oppdaterfarge;
-				colorBlueElement.onmousemove = oppdaterfarge;
+    colorRedElement.onmousemove = oppdaterfarge;
+    colorGreenElement.onmousemove = oppdaterfarge;
+    colorBlueElement.onmousemove = oppdaterfarge;
 
-				colorRedElement.max = 255;
-				colorGreenElement.max = 255;
-				colorBlueElement.max = 255;
+    colorRedElement.max = 255;
+    colorGreenElement.max = 255;
+    colorBlueElement.max = 255;
 
-				colorRedElement.min = 0;
-				colorGreenElement.min = 0;
-				colorBlueElement.min = 0;
-				colorRedElement.value = 255;
-				colorGreenElement.value = 0;
-				colorBlueElement.value = 255;
-				colorRed = colorRedElement.value;
-				colorGreen = colorGreenElement.value;
-				colorBlue = colorBlueElement.value;
-				fargeFraFargevelger = "rgb(" + colorRed + "," + colorGreen + "," + colorBlue + ")";
-				document.getElementById("boks").style.backgroundColor = fargeFraFargevelger;
-			}
-			function oppdaterfarge() {
-				colorRed = colorRedElement.value;
-				colorGreen = colorGreenElement.value;
-				colorBlue = colorBlueElement.value;
-				fargeFraFargevelger = "rgb(" + colorRed + "," + colorGreen + "," + colorBlue + ")"
-				document.getElementById("boks").style.backgroundColor = fargeFraFargevelger;
-			}
+    colorRedElement.min = 0;
+    colorGreenElement.min = 0;
+    colorBlueElement.min = 0;
+    colorRedElement.value = 255;
+    colorGreenElement.value = 0;
+    colorBlueElement.value = 255;
+    colorRed = colorRedElement.value;
+    colorGreen = colorGreenElement.value;
+    colorBlue = colorBlueElement.value;
+    fargeFraFargevelger = "rgb(" + colorRed + "," + colorGreen + "," + colorBlue + ")";
+    document.getElementById("boks").style.backgroundColor = fargeFraFargevelger;
+}
 
-			function settFarge() {
-				skalSetteFarge = true
-				console.log("true")
-			}
-			function giFarge(event) {
-				console.log(skalSetteFarge)
-				if(skalSetteFarge === true){
-				console.log(this)
-				this.style.backgroundColor = fargeFraFargevelger;
-				skalSetteFarge = false;
-				}
-			}
+function oppdaterfarge() {
+    colorRed = colorRedElement.value;
+    colorGreen = colorGreenElement.value;
+    colorBlue = colorBlueElement.value;
+    fargeFraFargevelger = "rgb(" + colorRed + "," + colorGreen + "," + colorBlue + ")"
+    document.getElementById("boks").style.backgroundColor = fargeFraFargevelger;
+}
+
+function settFarge() {
+    skalSetteFarge = true
+    console.log("true")
+}
+
+function giFarge(event) {
+    console.log(skalSetteFarge)
+    if (skalSetteFarge === true) {
+        console.log(this)
+        this.style.backgroundColor = fargeFraFargevelger;
+        skalSetteFarge = false;
+    }
+}
