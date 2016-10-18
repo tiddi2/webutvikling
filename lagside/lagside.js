5c8ba0516dd194546dcabb131c6abd3a1761ca81
MariusMOlsen <mariusol@hiof.no>
2016-10-18 19:41:25 +0200

    fikset border funksjonen og la til tilhørende range inputs

--- lagside/lagside.js
+++ lagside/lagside.js
@@ -12,10 +12,41 @@ var colorVisualiser = document.getElementById("fargeEndrer")
 var fargeFraFargevelger;
 var skalSetteFarge = false;
 
+// For å resete skyggen
+document.getElementById("btnSkyggeReset").onclick = resetSkygge;
+// for å oppdatere størrelsene på skyggen
+document.getElementById("skyggeBredde").onmousemove = hentStorrelse;
+document.getElementById("skyggeHoyde").onmousemove = hentStorrelse;
+document.getElementById("hvorIgjennomsiktig").onmousemove = hentStorrelse;
+document.getElementById("spread").onmousemove = hentStorrelse;
+				
+// elementer til skygge
+document.getElementById("skyggeBredde").min=-50;
+document.getElementById("skyggeHoyde").min=-50;
+document.getElementById("spread").min = 0;
+document.getElementById("hvorIgjennomsiktig").min=0;
+				
+document.getElementById("skyggeBredde").max= 50;
+document.getElementById("skyggeHoyde").max =50;
+document.getElementById("spread").max = 50;
+document.getElementById("hvorIgjennomsiktig").max=100;
+				
+document.getElementById("skyggeBredde").value= 0;
+document.getElementById("skyggeHoyde").value =0;
+document.getElementById("spread").value = 0;
+document.getElementById("hvorIgjennomsiktig").value=0;
+
 //Border elemeneter
-var borderStyleSelektorElement = document.getElementById("borderStyleSelektor")
-var borderVisualiser = document.getElementById("borderVisualiser")
-var borderTykkelse = document.getElementById("borderTykkelse")
+var borderStyleSelektorElement = document.getElementById("borderStyleSelektor");
+var borderVisualiser = document.getElementById("borderVisualiser");
+var borderTykkelse = document.getElementById("borderTykkelse");
+var shadowWidth = document.getElementById("skyggeBredde");
+var shadowHeight = document.getElementById("skyggeHoyde");
+var shadowBlur = document.getElementById("hvorIgjennomsiktig");	
+var shadowSpread = document.getElementById("spread");
+var skalSetteBorder = false;
+var velgBorderElement;
+
 
 //brukerstyrte funksjoner
 colorVisualiser.click = settFarge;
@@ -23,8 +54,7 @@ colorRedElement.oninput = oppdaterfarge;
 colorGreenElement.oninput = oppdaterfarge;
 colorBlueElement.oninput = oppdaterfarge;
 colorAlphaElement.oninput = oppdaterfarge;
-borderTykkelse.oninput = oppdaterBorder
-borderStyleSelektorElement.onchange = oppdaterBorder
+
 document.getElementById("fontVelger").onchange = velgFont;
 document.getElementById("fontSize").onchange = velgFontSize;
 document.getElementById("tittelFelt").onclick = sistValgtTekstfelt;
@@ -40,6 +70,14 @@ document.getElementById("sectionOverskrift3").onclick = sistValgtTekstfelt;
 document.getElementById("artikkelOverskrift1").onclick = sistValgtTekstfelt;
 document.getElementById("artikkelOverskrift2").onclick = sistValgtTekstfelt;
 document.getElementById("artikkelOverskrift3").onclick = sistValgtTekstfelt;
+
+borderTykkelse.oninput = oppdaterBorder
+borderStyleSelektorElement.onchange = oppdaterBorder
+borderStyleSelektorElement.onchange = oppdaterBorder2
+document.getElementById("border1").onclick = giBorder;
+document.getElementById("border2").onclick = giBorder;
+document.getElementById("border3").onclick = giBorder;
+document.getElementById("border4").onclick = giBorder;
 
 
 
@@ -79,11 +117,67 @@ function oppdaterfarge() {
     colorVisualiser.style.backgroundColor = fargeFraFargevelger;
 }
 
+// Borderfunksjoner
 function oppdaterBorder() {
 	borderVisualiser.style.border = borderStyleSelektorElement.options[borderStyleSelektorElement.selectedIndex].text
 	borderVisualiser.style.borderWidth  = borderTykkelse.value + "px"
 }
 
+function oppdaterBorder2() {
+	document.getElementById(velgBorderElement).style.border = borderStyleSelektorElement.options[borderStyleSelektorElement.selectedIndex].text
+	document.getElementById(velgBorderElement).style.borderWidth  = borderTykkelse.value + "px"
+}
+
+
+function giBorder(evt){
+	
+	velgBorderElement = this.id;
+	oppdaterBorder2()
+	hentStorrelse();
+	hentStorrelse2();
+	
+
+}
+// border funksjoner slutt
+
+// Endring av skyggen
+
+function hentStorrelse(){
+shadowWidth = document.getElementById("skyggeBredde");
+shadowHeight = document.getElementById("skyggeHoyde");
+shadowBlur = document.getElementById("hvorIgjennomsiktig");
+shadowSpread = document.getElementById("spread");
+				
+	if (shadowWidth.value!=="" && shadowHeight.value!=="" && shadowBlur.value!=="") {
+	document.getElementById("borderVisualiser").style.boxShadow= shadowWidth.value+"px " + shadowHeight.value+"px " + shadowBlur.value +"px " + shadowSpread.value +"px " + "#888888";
+					
+	} 
+console.log(shadowWidth.value+","+ shadowHeight.value+","+ shadowBlur.value);
+}
+
+function hentStorrelse2(){
+shadowWidth = document.getElementById("skyggeBredde");
+shadowHeight = document.getElementById("skyggeHoyde");
+shadowBlur = document.getElementById("hvorIgjennomsiktig");
+shadowSpread = document.getElementById("spread");
+				
+	if (shadowWidth.value!=="" && shadowHeight.value!=="" && shadowBlur.value!=="") {
+	document.getElementById(velgBorderElement).style.boxShadow= shadowWidth.value+"px " + shadowHeight.value+"px " + shadowBlur.value +"px " + shadowSpread.value +"px " + "#888888";
+					
+	} 
+console.log(shadowWidth.value+","+ shadowHeight.value+","+ shadowBlur.value);
+}
+
+
+// resetter skyggen
+function resetSkygge() {
+document.getElementById("skyggeBredde").value= 0;
+document.getElementById("skyggeHoyde").value =0;
+document.getElementById("spread").value = 0;
+document.getElementById("hvorIgjennomsiktig").value=0;
+hentStorrelse();
+hentStorrelse2();
+}
 
 
 //Text edit funksjoner
